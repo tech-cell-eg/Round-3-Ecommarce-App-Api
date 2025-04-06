1659b5c911c3454df37739ab46ab127a59838ef6
Tarek Hesham <tarekghost6@gmail.com>
2025-04-06 22:38:31 +0200

    [Product Listings] - Create api.php file and register them in app.php

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
