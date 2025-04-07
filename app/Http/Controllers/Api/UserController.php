4f38f60a10e3721bd9c7d342982583c81a0d8a94
Abdomoh <abdoshmhomed@gmail.com>
2025-04-07 02:40:26 +0100

    add user registration, login, logout and user listing

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    use ApiResponser;
+
+    public function index(){
+        $users=User::orderBy('id','desc')->paginate(5);
+        return $this->success($users);
+    }
+    public function store(StoreUserRequest $request){
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' =>$data['password'],
+        ]);
+
+        $token = $user->createToken($user->name . 'AuthToken')->plainTextToken;
+        return $this->success([$user, 'access_token' => $token], __('main.register_customer_is_done'));
+
+
+    }
+
+    public function login(Request $request)
+
+    {
+        $input = $request->validate([
+            'email' => 'required',
+            'password' => 'required|'
+        ]);
+        $user = User::where('email', $input['email'])->first();
+        if (!$user || !Hash::check($input['password'], $user->password)) {
+
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        } else {
+            $token = $user->createToken($user->name . 'AuthToken')->plainTextToken;
+            return $this->success([$user, 'user_type' => $user->user_type, 'access_token' => $token], __('main.login_authenticated_is_done'));
+        }
+    }
+
+    public function logout(Request $request)
+    {
+       
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            "message" => __('main.log_out')
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ProductController;
 
+
+// authenticated end points
+Route::controller(UserController::class)->prefix('users')->group(function(){
+    Route::get('display','index');
+    Route::post('/register','store');
+    Route::post('/login','login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
+
 // Products end points
-Route::get('products', [ProductController::class, 'index']);
+Route::get('products', [ProductController::class, 'index'])->middleware('auth:sanctum');
 Route::get('products/{id}', [ProductController::class, 'show']);
 
 // Cart end points
 Route::get('cart', [CartController::class, 'index']);
 Route::post('cart', [CartController::class, 'store']);
+
+
