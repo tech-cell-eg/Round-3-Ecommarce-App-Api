4f5f978e92442438bf1ac4e4152167b695cd15c1
Tarek Hesham <tarekghost6@gmail.com>
2025-04-07 19:14:40 +0200

    [Refactor] - Use validations and collections in cart controller

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -2,50 +2,51 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\ApiResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCartRequest;
+use App\Http\Resources\CartResource;
 use App\Models\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
+    use ApiResponse;
+
     public function index(Request $request)
     {
-        // Get all the products in the cart for all users
-        return response()->json([
-            // dont foget to replace $request->user_id with Auth::id() when using authentication
-            'data' => Cart::where('user_id', $request->user_id)->with('product')->get(),
-            'success' => true
-        ]);
+        /* 
+        ** Get all the products in the cart for all users
+        ** dont foget to replace $request->user_id with Auth::id() when using authentication
+        */
+        return $this->successResponse(
+            CartResource::collection(Cart::where('user_id', $request->user_id)->with('product')->get()),
+            'Fetched successfully'
+        );
     }
 
-    public function store(Request $request)
+    public function store(StoreCartRequest $request)
     {
-        // Find if the product is already in the cart and update the quantity for the user
-        // and dont foget to replace $request->user_id with Auth::id() when using authentication
+        /* 
+        ** Find if the product is already in the cart and update the quantity for the user
+        ** and dont foget to replace $request->user_id with Auth::id() when using authentication
+        */
+
         $product = Cart::where('product_id', $request->product_id)->where('user_id', $request->user_id)->first();
 
         if ($product) {
             $product->quantity += $request->quantity;
             $product->save();
-            return response()->json([
-                'data' => $product,
-                'success' => true
-            ]);
+            return $this->successResponse($product, 'Product updated successfully');
         }
 
-        // Create a new cart with validation
-        $validated = $request->validate([
-            'user_id' => 'required',
-            'product_id' => 'required',
-            'quantity' => 'required|numeric|min:1',
+        $cart = Cart::create([
+            'user_id' => $request->user_id,
+            'product_id' => $request->product_id,
+            'quantity' => $request->quantity
         ]);
 
-        $cart = Cart::create($validated);
-
-        return response()->json([
-            'data' => $cart,
-            'success' => true
-        ]);
+        return $this->successResponse($cart, 'Product added to cart successfully');
     }
 }
