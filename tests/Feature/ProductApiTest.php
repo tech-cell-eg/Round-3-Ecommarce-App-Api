085dd4a7ed939d88ee48561c0b1f9364d7fdb582
Tarek Hesham <tarekghost6@gmail.com>
2025-04-07 22:21:53 +0200

    [Testing] -  Create unit testing for product end-points

---
+++ tests/Feature/ProductApiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductApiTest extends TestCase
+{
+    use RefreshDatabase; // Uncomment this line to reset all data before testing
+
+    public function test_can_get_all_products()
+    {
+        Product::factory()->count(3)->create();
+
+        $response = $this->getJson('/api/products');
+
+        $response->assertStatus(200)->assertJsonCount(3);
+    }
+
+    public function test_can_get_single_product()
+    {
+        $product = Product::factory()->create();
+
+        $response = $this->getJson('/api/products/' . $product->id);
+
+        $response->assertStatus(200)
+                ->assertJsonFragment([
+                    'id' => $product->id,
+                    'title' => $product->title,
+                ]);
+    }
+}
