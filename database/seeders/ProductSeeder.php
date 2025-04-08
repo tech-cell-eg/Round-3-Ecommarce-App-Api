cdf32bb4855c8ab77f54e380cd245ca010004c7d
Tarek Hesham <tarekghost6@gmail.com>
2025-04-08 21:45:18 +0200

    [Product Categories] - Create factory and seeder for dummy categories.

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
@@ -13,6 +14,11 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory(10)->create();
+        $categories = Category::factory(10)->create();
+
+        Product::factory(20)->create()->each(function ($product) use ($categories) {
+            $assignedCategories = $categories->random(rand(1, 4))->pluck('id')->toArray();
+            $product->categories()->sync($assignedCategories);
+        });
     }
 }
