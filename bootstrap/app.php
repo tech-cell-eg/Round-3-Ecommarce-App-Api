7016743dea1801ceb14ef0414572480bb298fff6
Tarek Hesham <tarekghost6@gmail.com>
2025-04-09 21:33:12 +0200

    [Refactor] - ForceResponse to JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\CorsMiddleware;
+use App\Http\Middleware\ForceJsonResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,9 +23,32 @@ return Application::configure(basePath: dirname(__DIR__))
             'is_admin' => AdminMiddleware::class
         ]);
 
+        // This will add the ForceJsonResponse middleware to the beginning of the middleware stack
+        $middleware->prepend(ForceJsonResponse::class);
+
         // This will add the CORS middleware to the end of the middleware stack
         $middleware->append(CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'not found'
+                ], 404);
+            }
+        });
+        $exceptions->render(function (AccessDeniedHttpException $e, Request $request){
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'forbidden'
+                ], 403);
+            }
+        });
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request){
+            if ($request->is('api/*')) {
+                return response()->json([
+                    "message" => $e->getMessage(),
+                ], 405);
+            }
+        });
     })->create();
