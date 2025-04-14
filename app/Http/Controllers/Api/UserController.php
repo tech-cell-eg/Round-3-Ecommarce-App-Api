86929240c2636f692aaf17fa8319ef74c4f966fe
Tarek Hesham <tarekghost6@gmail.com>
2025-04-14 14:25:40 +0200

    [Improvement] Enhanced user registration & login with custom validation and response with Resource

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,8 +7,10 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -24,39 +26,60 @@ class UserController extends Controller
     {
         $data = $request->validated();
 
+        if ($request->hasFile('avatar')) {
+            $data['avatar'] = 'storage/' . $request->file('avatar')->store('avatars', 'public');
+        } else {
+            $data['avatar'] = asset('avatars/user-avatar.png');
+        }        
+
         $user = User::create([
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'email' => $data['email'],
-            'password' => $data['password'],
-            'user_type' => 'user'
+            'first_name'   => $data['first_name'],
+            'last_name'    => $data['last_name'],
+            'email'        => $data['email'],
+            'phone_number' => $data['phone_number'],
+            'password'     => $data['password'],
+            'avatar'       => $data['avatar'],
+            'user_type'    => 'user',
         ]);
 
-        $token = $user->createToken($user->first_name . $user->last_name . 'AuthToken')->plainTextToken;
-        return $this->successResponse([$user, 'access_token' => $token], __('main.register_customer_is_done'));
+        $user->access_token = $user->createToken("{$user->first_name}-{$user->id}-{$user->last_name}")->plainTextToken;
+
+        return $this->successResponse(
+            new UserResource($user),
+            "User created successfully"
+        );
     }
 
-    public function login(Request $request)
-
+    public function login(LoginRequest $request)
     {
-        $input = $request->validate([
-            'email' => 'required',
-            'password' => 'required|'
-        ]);
+        $credentials = $request->validated();
 
-        $user = User::where('email', $input['email'])->first();
-        if (!$user || !Hash::check($input['password'], $user->password)) {
-            return $this->errorResponse('Invalid Credentials', 401);
-        } else {
-            $token = $user->createToken($user->first_name . $user->last_name . 'AuthToken')->plainTextToken;
-            return $this->successResponse([$user, 'user_type' => $user->user_type, 'access_token' => $token], __('main.login_authenticated_is_done'));
+        $user = User::where('email', $credentials['email'])->first();
+
+        // Check if user not found
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return $this->errorResponse("Invalid credentials", 401);
         }
+
+        // Create user token
+        $user->access_token = $user->createToken("{$user->first_name}{$user->last_name}AuthToken")->plainTextToken;
+
+        return $this->successResponse(
+            new UserResource($user),
+            "Logged in successfully"
+        );
     }
 
     public function logout(Request $request)
     {
+        // Check if user not authenticated
+        if (!$request->user()) {
+            return $this->errorResponse("Unauthenticated", 401);
+        }
 
+        // Delete user tokens
         Auth::user()->tokens()->delete();
-        return $this->successResponse(null, __('main.log_out'));
+
+        return $this->successResponse(null, "Logged out successfully");
     }
 }
